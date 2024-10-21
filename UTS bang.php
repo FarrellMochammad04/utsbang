73889a1b27e92a3e3f646e988d7f75da0599cb99
FarrellMochammad04 <144803971+FarrellMochammad04@users.noreply.github.com>
2024-10-21 10:00:53 +0700

    Add files via upload

---
+++ UTS bang.php
@@ -0,0 +1,20 @@
+[
+  {
+    "id": 1,
+    "name": "Espresso",
+    "description": "Strong and bold coffee.",
+    "price": 3.00
+  },
+  {
+    "id": 2,
+    "name": "Latte",
+    "description": "Creamy coffee with steamed milk.",
+    "price": 4.00
+  },
+  {
+    "id": 3,
+    "name": "Cappuccino",
+    "description": "Coffee with frothed milk.",
+    "price": 4.50
+  }
+]
